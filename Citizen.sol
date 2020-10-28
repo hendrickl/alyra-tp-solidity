16da929c341a82ed6ad5d99b669898186693de2d
hendrick <h.lincertin@gmail.com>
2020-10-28 23:18:58 +0100

    Citizen [smart contract] v0.2

--- Citizen.sol
+++ Citizen.sol
@@ -8,7 +8,17 @@ contract Citizen {
         string gender;
         bool courtMember;
         bool councilMember;
+        bool banned;
     }
 
+    // _memberAddress: Address of the member
+    address public _memberAddress;
+
+    // _balancesMembers: Mapping FROM account addresses TO current balance.
+    mapping(address => uint256) private _balancesMembers;
+
     constructor() public {}
+
+    // setMember() adds a new member
+    function setMember() public {}
 }

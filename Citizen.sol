545cd8bee338c6d9c2c2c16dec34d99598204d92
hendrick <h.lincertin@gmail.com>
2020-10-29 17:46:40 +0100

    Citizen [smart contract] v0.5

--- Citizen.sol
+++ Citizen.sol
@@ -2,11 +2,9 @@
 pragma solidity ^0.6.0;
 
 contract Citizen {
-    // _infoCitizen: Mapping FROM account addresses TO current informations.
-    mapping(address => InfoCitizen) private _infoCitizen;
-
     // InfoCitizen returns informations about the citizen.
     struct InfoCitizen {
+        //address payable citizen;
         string gender;
         uint256 age;
         bool courtMember;
@@ -14,8 +12,8 @@ contract Citizen {
         bool banned;
     }
 
-    // _citizenAccount: Address of the citizen.
-    address public citizenAccount;
+    // infoCitizens: Mapping FROM account addresses TO current informations.
+    mapping(address => InfoCitizen) public infoCitizens;
 
     // _balancesCitizens: Mapping FROM account addresses TO current balance.
     mapping(address => uint256) public balancesCitizens;
@@ -31,17 +29,12 @@ contract Citizen {
         bool _councilMember,
         bool _banned
     ) public {
-        _infoCitizen[_address] = InfoCitizen(
+        infoCitizens[_address] = InfoCitizen(
             _gender,
             _age,
             _courtMember,
             _councilMember,
             _banned
         );
-    }
-
-    // getCitizen() returns a informations struct about the account.
-    function getCitizen(address _address) public view {
-        _infoCitizen[_address];
     }
 }

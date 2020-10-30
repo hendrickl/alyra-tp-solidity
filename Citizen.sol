bf1d2f3dd769cf0dcd5165eaa39814e052a97c72
hendrick <h.lincertin@gmail.com>
2020-10-30 02:37:58 +0100

    Citizen [smart contract] v0.6

--- Citizen.sol
+++ Citizen.sol
@@ -1,19 +1,26 @@
+// Harry : 0x1F4FDad26cfe9636ADB5595A3814A25e892A9326
+// Tracy : 0xF034a2AA883D2909AF9B3c7406ed01d6a2dF80ca
+// Mum : 0x6C97b80D51F0d13744419c48a9045456c82f14dA
+// State : 0x65c2c71FB6b78d07dc1Adc81ecdaC7983A5572D9
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 import "ERCcitizen.sol";
 
 contract Citizen {
-    // owner
-    address ownerCitizen = payable(0xdD870fA1b7C4700F2BD7f44238821C26f7392148);
+    // ownerCitizen: Address of the owner, used for administrative and sensitive function.
+    address public ownerCitizen;
+
+    // _count and _step accounts the population.
+    uint256 private _count;
+    uint256 private _step;
 
     // InfoCitizen returns informations about the citizen.
     struct InfoCitizen {
-        //address payable citizen;
         string gender;
         uint256 age;
         bool courtMember;
         bool councilMember;
-        bool banned;
+        mapping(address => uint256) balancesCitizens;
     }
 
     // infoCitizens: Mapping FROM account addresses TO current administrative informations.
@@ -22,7 +29,20 @@ contract Citizen {
     // _balancesCitizens: Mapping FROM account addresses TO current balance.
     mapping(address => uint256) public balancesCitizens;
 
-    constructor() public {}
+    constructor() public {
+        ownerCitizen = 0x65c2c71FB6b78d07dc1Adc81ecdaC7983A5572D9;
+        _count = 0;
+        _step = 1;
+    }
+
+    // A modifier for checking if the `msg.sender` is the owner.
+    modifier onlyOwnerCitizen {
+        require(
+            msg.sender == ownerCitizen,
+            "Citizen: Only owner can perform this action"
+        );
+        _;
+    }
 
     // setCitizen() adds a new citizen.
     function setCitizen(
@@ -30,15 +50,32 @@ contract Citizen {
         string memory _gender,
         uint8 _age,
         bool _courtMember,
-        bool _councilMember,
-        bool _banned
-    ) public {
+        bool _councilMember
+    ) public onlyOwnerCitizen() {
         infoCitizens[_address] = InfoCitizen(
             _gender,
             _age,
             _courtMember,
-            _councilMember,
-            _banned
+            _councilMember
         );
+    }
+
+    // mapCitizen() sets 100 CTZ in the amount of the welcoming.
+    function mapCitizen(address _address) public onlyOwnerCitizen {
+        balancesCitizens[_address] = 100;
+    }
+
+    // increment() and decrease() manage the number of citizens.
+    function increment() public onlyOwnerCitizen {
+        _count += _step;
+    }
+
+    function decrease() public onlyOwnerCitizen {
+        _count -= _step;
+    }
+
+    // nbPopulation returns the total of citizens.
+    function nbPopulation() public view returns (uint256) {
+        return _count;
     }
 }

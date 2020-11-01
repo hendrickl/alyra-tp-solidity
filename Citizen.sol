b2a64fde369175fc8aefca5918142ca91acb998e
hendrick <h.lincertin@gmail.com>
2020-11-01 03:00:26 +0100

    Citizen [function register fixed] v0.7

--- Citizen.sol
+++ Citizen.sol
@@ -1,21 +1,19 @@
-// Harry : 0x1F4FDad26cfe9636ADB5595A3814A25e892A9326
-// Tracy : 0xF034a2AA883D2909AF9B3c7406ed01d6a2dF80ca
-// Mum : 0x6C97b80D51F0d13744419c48a9045456c82f14dA
-// State : 0x65c2c71FB6b78d07dc1Adc81ecdaC7983A5572D9
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 import "ERCcitizen.sol";
 
 contract Citizen {
-    // ownerCitizen: address of the owner, used for administrative and sensitive function.
+    // _ownerCitizen: address of the owner, used for administrative and sensitive function.
     address private _ownerCitizen;
 
-    // addressCitizen: address of the current citizen
+    // _addressCitizen: address of a citizen registered by owner.
     address private _addressCitizen;
 
     // peopleCount is a counter of the population.
     // peopleCount is also a citizen ID.
-    uint256 public peopleCount;
+    uint256 private _peopleCount;
+
+    uint256 private _id;
 
     // StatusCtn returns status informations about the citizen.
     struct StatusCtn {
@@ -46,23 +44,23 @@ contract Citizen {
     // ID => addressCitizen
     mapping(uint256 => address) public citizenAddr;
 
-    // citizenId: Mapping FROM a counter TO an id.
-    // peopleCount => ID
-    mapping(uint256 => uint256) public citizenId;
+    // citizenId: Mapping FROM a registered citizen address TO an id.
+    // addressCitizen => ID
+    mapping(address => uint256) public citizenId;
 
     // balancesCitizens: Mapping FROM account addresses TO current balance.
+    // addressCitizen => balanceCitizen
     mapping(address => uint256) public balancesCitizens;
 
     constructor() public {
-        _ownerCitizen = 0x65c2c71FB6b78d07dc1Adc81ecdaC7983A5572D9;
-        //_addressCitizen = citizenAddr[peopleCount];
-        citizenId[peopleCount] = peopleCount;
-        citizenAddr[peopleCount] = _addressCitizen;
-        peopleCount = 0;
+        _ownerCitizen = address(0x65c2c71FB6b78d07dc1Adc81ecdaC7983A5572D9);
+        citizenAddr[_peopleCount] = _addressCitizen;
+        citizenId[_addressCitizen] = _peopleCount;
+        _peopleCount = 0;
     }
 
     // A modifier for checking if the `msg.sender` is the owner.
-    modifier onlyOwnerCitizen {
+    modifier onlyOwnerState {
         require(
             msg.sender == _ownerCitizen,
             "Citizen: Only owner can perform this action"
@@ -70,53 +68,64 @@ contract Citizen {
         _;
     }
 
-    // addCitizen() adds a new citizen.
-    // addCitizen() uses the struct InfoCitizen and fill the mapping infoCitizens.
-    function addCitizen(
-        address _address,
-        bool _courtMember,
-        bool _councilMember,
-        string memory _companyMember
-    ) public onlyOwnerCitizen() {
-        peopleCount += 1;
-        citizenAddr[peopleCount] = _address;
-        citizenId[peopleCount] = peopleCount;
-        statusCitizens[_address] = StatusCtn(
-            peopleCount,
-            _courtMember,
-            _councilMember,
-            _companyMember
-        );
-    }
-
-    modifier onlyCitizen {
+    // A modifier for checking if the `msg.sender` is a citizen.
+    modifier onlyOwnerCitizen {
         require(
-            msg.sender == _addressCitizen,
+            citizenAddr[_peopleCount] == msg.sender,
             "Citizen: Only citizen can perform this action"
         );
         _;
     }
 
+    // setCitizen() sets a new citizen.
+    // setCitizen() uses the struct InfoCitizen and fills the mapping infoCitizens.
+    function setCitizen(
+        address _address,
+        bool _courtMember,
+        bool _councilMember,
+        string memory _companyMember
+    ) public onlyOwnerState() {
+        _addressCitizen = _address;
+        _peopleCount += 1;
+        citizenId[_address] = _peopleCount;
+        citizenAddr[_peopleCount] = _address;
+        statusCitizens[_address] = StatusCtn(
+            _peopleCount,
+            _courtMember,
+            _councilMember,
+            _companyMember
+        );
+        // +EVENT
+    }
+
     // register() adds the identity of the citizen.
     // register() uses the struct Identity and fills the mapping idCitizens.
     function register(
+        uint256 _idCtn,
         string memory _firstName,
         string memory _lastName,
         string memory _gender,
         uint256 _age,
         string memory _location
-    ) public onlyCitizen {
-        idtCitizens[peopleCount] = Identity(
+    ) public onlyOwnerCitizen {
+        _id = _idCtn;
+        idtCitizens[_id] = Identity(
             _firstName,
             _lastName,
             _gender,
             _age,
             _location
         );
+        // +EVENT
+    }
+
+    // allCitizens() returns the number of citizens.
+    function allCitizens() public view returns (uint256) {
+        return _peopleCount;
     }
 
     // transferCtz() sends 100 CTZ in the account of the welcomed citizen.
-    function transferCtz(address _address) public onlyOwnerCitizen {
+    function transferCtz(address _address) public onlyOwnerState {
         balancesCitizens[_address] = 100;
     }
 }
